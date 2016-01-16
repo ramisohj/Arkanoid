dcc58d966559c35fffcc38bc684230fe378d266c
unknown <ramisohj@gmail.com>
2016-01-16 23:45:49 +0100

    She is Always Ready

---
+++ Arkanoid_14.0/src/Game/Main.java
@@ -0,0 +1,183 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import static java.lang.Thread.sleep;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author R@MiSoHj
+ */
+public class Main extends JFrame{
+    
+    private Ball ball = new Ball();
+   
+    
+    private Wall wall = new Wall();
+    
+    private Ship ship;
+    private Image imageDB;
+    private Graphics graphDB;
+    
+    //final width = 1366 ;
+    //final height = 744 ;
+    private final int width = 1000;
+    private final int height = 744;
+    private Sound sound;
+    
+    
+    Image imageMenu;
+    Graphics gm ;
+
+    public void init() {
+        //final width = 1366 ;
+        //final height = 744 ;
+        this.setTitle("ARKANOID");
+        this.setSize(width+300, height);
+        this.setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e){
+                super.keyPressed(e); 
+                ship.addMove(e);
+            }
+            
+        });    
+        imageDB = new BufferedImage(getWidth()+300, getHeight(), BufferedImage.TYPE_INT_RGB);
+        graphDB = imageDB.getGraphics();
+
+        sound = new Sound();
+        run();
+    }
+    
+    
+    public void paint(Graphics g){
+        g.drawImage(imageDB,0,0, this); 
+    }
+     
+    int min;
+    public void run() {
+        try{
+        ball = new Ball(10, 500, 500,-1,-1,Color.white);
+        ship = new Ship(500, 500, 120, 30, Color.cyan);
+        
+        wall.fillBockList();
+        wall.blockListUpdatepoints();
+        //sound.soundtrack();
+        
+        margin();
+        while(true){
+            System.out.println(min);
+            //playMusic();
+            try {
+            //System.out.println("\n");    
+            //graphDB.fillRect(0,0, width, height);
+            graphDB.clearRect(0,47, getWidth()-290, getHeight()-73);
+            
+            //CRASHES
+            
+            borderCrash(ball);
+            ship.shipCrash(ball);
+            wall.blockListCrashes(ball);
+            
+            //UPDATE ELEMENTS
+            wall.blockListKill(ship);
+            ball.wayCalculator();
+            
+            ship.move();
+            ship.updateGuns();        
+            ship.paintAllGuns(graphDB);
+            ship.updatePoints();//save ship points
+            
+            //PAINT ELEMENTS
+            ship.paintShip(graphDB);
+            ball.paintBall(graphDB,ball.getRadio(), ball.getXC(), ball.getYC());  
+            wall.paintBlockList(graphDB);
+            //--wall.painBlockMaze(graphDB);
+            
+            //efects
+            wall.boomEfect(graphDB);
+            wall.boomListUpdate();
+            
+            repaint();
+            sleep(5);
+            } catch (InterruptedException ex) {
+                 System.out.println("there is a error  in the while.....!!!!");
+            }
+
+        }
+        }catch(Exception e){
+            System.out.println("there is a error!!!!");
+        }     
+    }
+    
+   @Override
+    public void update(Graphics g){
+        paint(g);
+    }
+    private void verifyCrashes(){
+        
+    }
+    private void playMusic(){
+        if(min >=11000){
+            sound.soundtrack();
+            min = 0;
+        }
+        min++;
+    }
+    
+    public void menu(){
+        
+        
+    }
+    public void margin(){
+       graphDB.setColor(Color.blue);
+       graphDB.fillRect(1000,0,1300,800);//Menu Block
+       graphDB.fillRect(0,0,1000,47);//top
+       graphDB.fillRect(0,720,1000,744);//below
+      
+    }
+    private void borderCrash(Ball ball){
+        if(ball.getYC()<=57 && ball.getDY()<0){
+            ball.setDY(ball.getDY()*-1);
+            ball.setAllFirst();
+            //sound.crash();
+        }
+        if(ball.getYC() >= (height-37) && ball.getDY()>0){
+            ball.setDY(ball.getDY()*-1);
+            ball.setAllFirst();
+            //sound.crash();
+        }
+        if(ball.getXC() <= 8 && ball.getDX()<0){
+            ball.setDX(ball.getDX()*-1);
+            ball.setAllFirst();
+            //sound.crash();
+        }
+        if(ball.getXC() >=(width -9) && ball.getDX()>0){
+            ball.setDX(ball.getDX()*-1);
+            ball.setAllFirst();
+            //sound.crash();
+        }       
+    }
+}
---
+++ Arkanoid_14.0/src/Game/Ship.java
@@ -0,0 +1,380 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author R@MiSoHj
+ */
+public class Ship extends Block{
+
+    
+    private Sound sound;
+    private ArrayList<Gun> gunList;
+    private ArrayDeque<Integer> movNave;
+    
+    
+    public Ship(int xo, int yo, int w, int h, Color c) {
+        super(xo, yo, w, h, c);
+        gunList = new ArrayList<Gun>();
+        movNave = new ArrayDeque<Integer>();
+        sound = new Sound();
+    }
+    
+    public void addMove(KeyEvent e){
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            movNave.push(e.getKeyCode());
+        }else
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            movNave.push(e.getKeyCode());
+        }else
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            movNave.push(e.getKeyCode());
+        }else
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            movNave.push(e.getKeyCode());
+        }else
+        if(e.getKeyCode() == KeyEvent.VK_SPACE){
+            movNave.push(e.getKeyCode());
+        }
+    }
+    
+    public void move(){    
+        if(!movNave.isEmpty()){
+            //s.move();
+            //System.out.println("\n");
+            int key = movNave.poll();
+            if(key == KeyEvent.VK_RIGHT){
+                setX(getX()+15);
+                //System.out.println("-------------------------------RIGHT-------------------------");
+            }else
+            if(key == KeyEvent.VK_LEFT){             
+                setX(getX()-15);
+                //System.out.println("-------------------------------LEFT-------------------------");
+            }else   
+            if(key == KeyEvent.VK_UP){
+                setY(getY()-15);
+                //System.out.println("-------------------------------UP-------------------------");
+            }else
+            if(key == KeyEvent.VK_DOWN){
+                setY(getY()+15);
+                //System.out.println("-------------------------------DOWN-------------------------");
+            }else
+            if(key == KeyEvent.VK_SPACE){
+                atack(Color.cyan);
+                //System.out.println("-------------------------------ATACK-------------------------");
+            }
+            }
+    }
+
+    public void atack(Color c){
+       int xm = ((x2-x1)/2)+x1;
+       int ym = y1;
+       //System.out.println("ATACK!!!!!!!!!!!!!!!!!!!!!!!");
+       sound.shotGun();
+       Gun g = new Gun(xm, ym, c);
+       gunList.add(g);
+    }
+    
+    public void paintAllGuns(Graphics g){
+        
+        for(Gun gun : gunList){
+            gun.goUp();
+        } 
+        if(!gunList.isEmpty()){
+            for(Gun gun : gunList){
+                //System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ATACK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                gun.paintGun(g);
+            }
+        }
+    }
+    
+    public void updateGuns(){
+        try{
+        int i = 0;
+        //System.out.println("Nro guns: ..............."+gunList.size());
+        for(Gun gun : gunList){
+            if(gun.getYM()<=47){
+                gunList.remove(gun);         
+            }
+            //System.out.println("Gun nro: "+i+";......................ym= "+gun.getYM()+"; xm= "+gun.getXM());  
+            i++;
+        }
+        }catch(Exception e){
+            System.out.println("error in the updateGuns!!!!");
+        }
+    }
+    public void paintShip(Graphics g){
+        super.paintBlock(g);
+        super.paintDavidStar(g,Color.cyan);
+    }
+    
+    public ArrayList<Gun> getGunlist(){
+        return gunList;
+    }
+    
+    //             X1       X2      X3     X4
+    //              | ______|_______|_______|
+    //              |                       |
+    //              |                       |
+    //              |_______________________|
+    //
+    private int xn1;
+    private int xn2;
+    private int xn3;
+    private int xn4;
+    private int div;
+    public void shipCrash( Ball ball){
+         div = super.getWidht()/3;
+         xn1 = super.getX();
+         xn2 = xn1+div;
+         xn3 = xn2+div;
+         xn4 = xn3+div;
+
+        if(ball.getDY()>=0){
+            crashTopToDown(ball);
+        }else{
+            crashDownToTop(ball);
+        }
+    }
+    //TOP TO DOWN
+    private void crashTopToDown(Ball ball){
+        crashTop(ball);
+        //crashCorner(ball,x1,y1,x2,y2);
+        /*
+        crashLeft(ball);
+        crashRight(ball);
+        */
+        if(ball.getDX()<0){
+            crashRight(ball);
+        }else{
+            crashLeft(ball);
+        }
+    }
+    //DOWN TO TOP
+    private void crashDownToTop(Ball ball){
+        crashDown(ball);
+        //crashCorner(ball,x3,y3,x4,y4);
+        if(ball.getDX()>0){
+            crashLeft(ball);
+        }else{
+            crashRight(ball);
+        }
+    }
+    
+    private void crashTop(Ball ball){
+        int r = (int)Math.pow(ball.getRadio(),2)-(int)Math.pow(sideY1-ball.getYC(),2);
+        double xp;
+        double xp1;
+        double xp2;
+        if(r == 0){
+            xp = ball.getXC();
+            if(xp>=sideX1 && xp<= sideX2 ){
+                
+                if(xp>=xn1 && xp<xn2){
+                    leftTopShip(ball);
+                }else
+                if(xp>=xn2 && xp<xn3){
+                    midTopShip(ball);
+                }else
+                if(xp>=xn3 && xp<=xn4){
+                    rightTopShip(ball);
+                }
+                
+                //ball.setDY(ball.getDY()*-1);
+               
+            }
+        }else
+        if(r > 0){
+            xp1 = Math.sqrt(r) + ball.getXC();
+            xp2 = -Math.sqrt(r) + ball.getXC();
+            if( (xp1>=sideX1 && xp1<= sideX2 ) ||
+                (xp2>=sideX1 && xp2<= sideX2 ) ){
+                 
+                if((xp1>=xn1 && xp1<xn2) || (xp2>=xn1 && xp2<xn2)){
+                    leftTopShip(ball);
+                }else
+                if((xp1>=xn2 && xp1<xn3) || (xp2>=xn2 && xp2<xn3)){
+                    midTopShip(ball);
+                }else
+                if((xp1>=xn3 && xp1<=xn4) || (xp2>=xn3 && xp2<xn4)){
+                    rightTopShip(ball);
+                }
+                //ball.setDY(ball.getDY()*-1);
+               
+            }       
+        }//else{
+        //    System.out.println("IMAGINARY NUMBERS!!!!!!!");
+        //}
+    }
+    private void leftTopShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(-1);
+            ball.setDX(2);
+        }else{
+            ball.setDY(-1);
+            ball.setDX(-2);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    private void midTopShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(-1);
+            ball.setDX(1);
+        }else{
+            ball.setDY(-1);
+            ball.setDX(-1);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    private void rightTopShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(-1);
+            ball.setDX(2);
+        }else{
+            ball.setDY(-1);
+            ball.setDX(-2);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    private void crashDown(Ball ball){
+        int r = (int)Math.pow(ball.getRadio(),2)-(int)Math.pow(sideY2-ball.getYC(),2);
+        double xp;
+        double xp1;
+        double xp2;
+        if(r == 0){
+            xp = ball.getXC();
+            if(xp>=sideX1 && xp<= sideX2 ){
+                if(xp>=xn1 && xp<xn2){
+                    rightBelowShip(ball);
+                }else
+                if(xp>=xn2 && xp<xn3){
+                    midBelowShip(ball);
+                }else
+                if(xp>=xn3 && xp<=xn4){
+                    leftBelowShip(ball);
+                }
+                
+            }
+        }else
+        if(r > 0){
+            xp1 = Math.sqrt(r) + ball.getXC();
+            xp2 = -Math.sqrt(r) + ball.getXC();
+            if( (xp1>=sideX1 && xp1<= sideX2 ) ||
+                (xp2>=sideX1 && xp2<= sideX2 ) ){
+                if((xp1>=xn1 && xp1<xn2) || (xp2>=xn1 && xp2<xn2)){
+                    rightBelowShip(ball);
+                }else
+                if((xp1>=xn2 && xp1<xn3) || (xp2>=xn2 && xp2<xn3)){
+                    midBelowShip(ball);
+                }else
+                if((xp1>=xn3 && xp1<=xn4) || (xp2>=xn3 && xp2<xn4)){
+                    leftBelowShip(ball);
+                }
+                 
+            }       
+        }//else{
+           // System.out.println("IMAGINARY NUMBERS!!!!!!!");
+        //}
+        
+    }
+    private void leftBelowShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(1);
+            ball.setDX(2);
+        }else{
+            ball.setDY(1);
+            ball.setDX(-2);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    private void midBelowShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(1);
+            ball.setDX(1);
+        }else{
+            ball.setDY(1);
+            ball.setDX(-1);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    private void rightBelowShip(Ball ball){
+        if(ball.getDX()>=0){
+            ball.setDY(1);
+            ball.setDX(2);
+        }else{
+            ball.setDY(1);
+            ball.setDX(-2);
+        }
+        ball.setAllFirst();
+        //sound.crash();
+    }
+    
+    
+    private void crashRight(Ball ball){
+        int r = (int)Math.pow(ball.getRadio(),2)-(int)Math.pow(sideX2-ball.getXC(),2);
+        double yp;
+        double yp1;
+        double yp2;
+        if(r == 0){
+            yp = ball.getYC();
+            if(yp>=sideY1 && yp<= sideY2 ){
+                ball.setDX(ball.getDX()*-1);
+                ball.setAllFirst();
+                //sound.crash();
+            }
+        }else
+        if(r > 0){
+            yp1 = Math.sqrt(r) + ball.getYC();
+            yp2 = -Math.sqrt(r) + ball.getYC();
+            if( (yp1>=sideY1 && yp1<= sideY2 ) ||
+                (yp2>=sideY1 && yp2<= sideY2 ) ){
+                ball.setDX(ball.getDX()*-1);
+                ball.setAllFirst();
+                //sound.crash();
+            }       
+        }//else{
+            //System.out.println("IMAGINARY NUMBERS!!!!!!!");
+       // }
+    }
+    private void crashLeft(Ball ball){
+        int r = (int)Math.pow(ball.getRadio(),2)-(int)Math.pow(sideX1-ball.getXC(),2);
+        double yp;
+        double yp1;
+        double yp2;
+        if(r == 0){
+            yp = ball.getYC();
+            if(yp>=sideY1 && yp<= sideY2 ){
+                ball.setDX(ball.getDX()*-1);
+                ball.setAllFirst();
+                //sound.crash();
+            }
+        }else
+        if(r > 0){
+            yp1 = Math.sqrt(r) + ball.getYC();
+            yp2 = -Math.sqrt(r) + ball.getYC();
+            if( (yp1>=sideY1 && yp1<= sideY2 ) ||
+                (yp2>=sideY1 && yp2<= sideY2 ) ){
+                ball.setDX(ball.getDX()*-1);
+                ball.setAllFirst();
+                //sound.crash();
+            }       
+        }else{
+            //System.out.println("IMAGINARY NUMBERS!!!!!!!");
+        }         
+    }
+}
